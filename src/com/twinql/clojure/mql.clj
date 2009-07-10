70500a469e5a8da1470145a51aa3223ec0f899b3
Richard Newman <Richard Newman  rnewman@franz.com>
2009-07-09 17:54:59 -0700

    Switch to using a separate Apache HTTP library.

--- src/com/twinql/clojure/mql.clj
+++ src/com/twinql/clojure/mql.clj
@@ -1,44 +1,12 @@
 (ns com.twinql.clojure.mql
   (:import 
     (java.lang Exception)
-    (java.net URI)
-    (org.apache.http.client.methods HttpGet HttpPost)
-    (org.apache.http.client HttpClient ResponseHandler)
-    (org.apache.http.client.utils URIUtils URLEncodedUtils)
-    (org.apache.http.message BasicNameValuePair)
-    (org.apache.http.impl.client DefaultHttpClient BasicResponseHandler))
+    (java.net URI))
   (:require 
+    [com.twinql.clojure.http :as http]
     [org.danlarkin.json :as json]))
 
-(def *server* (new URI "http://api.freebase.com/"))
-(def *query-path* "api/service/mqlread")
-
-(defn encode-query [q]
-  "q is a map or list of pairs."
-  (. URLEncodedUtils format
-     (map (fn [[param value]] 
-            (new BasicNameValuePair (str param) (str value)))
-          q)
-     "UTF-8"))
-
-(defn submit-query [host path q]
-  (let [encoded-query (when q (encode-query q))]
-    (let [whole-url (if encoded-query
-                      (. URIUtils resolve
-                         host
-                         (. URIUtils createURI nil nil 0 path encoded-query nil))
-                      host)]
-      (let [http-client (new DefaultHttpClient)
-            http-get    (new HttpGet whole-url)
-            response-handler (new BasicResponseHandler)]
-            
-        (let [response-body (. http-client execute http-get response-handler)]
-          (. (. http-client getConnectionManager) shutdown)
-          response-body)))))
-
-(defn envelope [query]
-  {"query" query
-   "escape" false})
+(def *query-server* (new URI "http://api.freebase.com/api/service/mqlread"))
 
 (defn process-query-result [res]
   (when res
@@ -46,16 +14,24 @@
       (if (= "/api/status/ok" (json :code))
         (json :result)
         (throw (new Exception
-                    (str "Non-OK status from MQL query: " (json :status))))))))
+                    (str "Non-OK status from MQL query: "
+                         (json :status))))))))
 
 (defn run-mql 
   ([mql debug]
-    (let [submitted (submit-query *server* *query-path*
-                                  {"query"
-                                   (json/encode-to-str
-                                     (envelope mql))})]
+    (let [[code response body] (http/get *query-server*
+                                         :query {"query"
+                                                 (json/encode-to-str
+                                                   {"query" mql
+                                                    "escape" false})}
+                                         :as :string)]
       (when debug
-        (print submitted))
-      (process-query-result submitted)))
+        (println "Got response" code response)
+        (prn body))
+      (if (and (>= code 200)
+               (< code 300))
+        (process-query-result body)
+        (throw (new Exception
+                    (str "Bad response: " code " " response))))))
   ([mql]
    (run-mql mql false)))
