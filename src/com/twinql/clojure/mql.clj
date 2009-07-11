8e0f78aafcf9e784891f3ddc16ac56e166929fe9
Richard Newman <Richard Newman  rnewman@franz.com>
2009-07-10 22:19:48 -0700

    Rename run-mql to mql-read. Add HTTP parameters. Allow multiple queries in one envelope. Add mql-version.

--- src/com/twinql/clojure/mql.clj
+++ src/com/twinql/clojure/mql.clj
@@ -6,32 +6,92 @@
     [com.twinql.clojure.http :as http]
     [org.danlarkin.json :as json]))
 
-(def *query-server* (new URI "http://api.freebase.com/api/service/mqlread"))
+(def *mql-version* (new URI "http://api.freebase.com/api/version"))
+(def *mql-read* (new URI "http://api.freebase.com/api/service/mqlread"))
 
-(defn process-query-result [res]
+(defn- process-multiple-query-results [res names]
   (when res
-    (let [json (json/decode-from-str res)]
-      (if (= "/api/status/ok" (json :code))
-        (json :result)
-        (throw (new Exception
-                    (str "Non-OK status from MQL query: "
-                         (json :status))))))))
+    (if (= "/api/status/ok" (:code res))
+      (map process-query-result
+           (vals (select-keys res (map keyword names))))
+      (throw (new Exception
+                  (str "Non-OK status from MQL query: "
+                       (:status res)))))))
 
-(defn run-mql 
-  ([mql debug]
-    (let [[code response body] (http/get *query-server*
-                                         :query {"query"
-                                                 (json/encode-to-str
-                                                   {"query" mql
-                                                    "escape" false})}
-                                         :as :string)]
-      (when debug
-        (println "Got response" code response)
-        (prn body))
-      (if (and (>= code 200)
-               (< code 300))
-        (process-query-result body)
-        (throw (new Exception
-                    (str "Bad response: " code " " response))))))
+(defn- process-query-result [res]
+  (when res
+    (if (= "/api/status/ok" (:code res))
+      (:result res)
+      (throw (new Exception
+                  (str "Non-OK status from MQL query: "
+                       (:status res)))))))
+
+(defmacro content
+  "Return only HTTP content."
+  [form]
+  `(~form 2))
+
+(defmacro debug-print [debug? value & msg]
+  `(when ~debug?
+     (println ~@msg)
+     (prn ~value)))
+
+(defmethod http/entity-as :json [entity as]
+  (json/decode-from-reader (http/entity-as entity :reader)))
+
+(defn envelope [q]
+  {"query" q
+   "escape" false})
+
+;; An infinite sequence of query names.
+(def query-names
+  (map (fn [i] (str "q" i)) (iterate inc 1)))
+
+(defn mql-read 
+  "Send a MQL query to Freebase. Optionally provide a dictionary of 
+  arguments suitable to http/get, and a boolean for debug output.
+  If a sequence of queries is provided, they are batched and run together;
+  the output is as if this function had been mapped over the sequence of
+  queries, but execution is more efficient."
+  ([mql http-options debug?]
+   (let [many? (sequential? mql)
+         names (when many?
+                 (take (count mql) query-names))
+         q (if many?
+             {"queries"
+              (json/encode-to-str
+                (zipmap names
+                        (map (comp envelope vector) mql)))}
+             {"query"
+              (json/encode-to-str
+                (envelope [mql]))})]
+     
+     (debug-print debug? q "Query parameters:")
+     (let [[code response body]
+           (http/get *mql-read*
+                     (:headers http-options)
+                     (:parameters http-options)
+                     :query q 
+                     :as :json)]
+
+       (debug-print debug? body "Got response" code response)
+       (if (and (>= code 200)
+                (< code 300))
+         
+         (if many?
+           (process-multiple-query-results body names)
+           (process-query-result body))
+
+         (throw (new Exception
+                     (str "Bad response: " code " " response)))))))
+  
+  ([mql http-options]
+   (mql-read mql http-options false))
+  
   ([mql]
-   (run-mql mql false)))
+   (mql-read mql {} false)))
+
+(defn mql-version
+  "Returns a map. Useful keys are :graph (graphd version), :me, :cdb, :relevance."
+  []
+  (content (http/get *mql-version* :as :json)))
