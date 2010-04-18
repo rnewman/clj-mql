0fe0e15725024cf70f81ae581a7ac47579e30830
Richard Newman <rnewman@twinql.com>
2010-04-17 18:52:21 -0700

    Dependency bumps.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject com.twinql.clojure/mql
-  "1.0.2"
+  "1.0.6"
   :description "Freebase MQL client library."
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
-                 [org.danlarkin/clojure-json "1.1-SNAPSHOT"] 
-                 [com.twinql.clojure/clj-apache-http "2.0.0"]])
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [org.danlarkin/clojure-json "1.1-SNAPSHOT"]
+                 ; [org.clojars.rnewman/clojure-json "1.2.2-SNAPSHOT-clojure120"] 
+                 [com.twinql.clojure/clj-apache-http "2.0.2"]])
 
